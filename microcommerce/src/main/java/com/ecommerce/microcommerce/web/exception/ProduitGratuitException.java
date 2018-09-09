ab275e292dff8e4361a3904d2f6f8fee333a67f4
Karimgdr <40438292+Karimgdr@users.noreply.github.com>
2018-09-09 21:12:11 +0200

    @ResponseStatus(HttpStatus.BAD_REQUEST)

--- microcommerce/src/main/java/com/ecommerce/microcommerce/web/exception/ProduitGratuitException.java
+++ microcommerce/src/main/java/com/ecommerce/microcommerce/web/exception/ProduitGratuitException.java
@@ -1,5 +1,9 @@
 package com.ecommerce.microcommerce.web.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ProduitGratuitException extends RuntimeException{
 	
 	public ProduitGratuitException(String s) {
